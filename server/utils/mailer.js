47fd32ec8612e05081b580d4db0b66181e1b873b
freezeflow <gfloriaan616@gmail.com>
2025-11-23 10:05:37 +0200

    Fix 1

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -3,6 +3,8 @@ import { RESEND_KEY, MAIL_FROM } from "../config.js";
 
 const resend = new Resend(RESEND_KEY);
 
+console.log("MAIL_FROM in PROD =", MAIL_FROM);
+
 export async function sendMail({ to, subject, html, attachments = [] }) {
   return await resend.emails.send({
     from: MAIL_FROM,
